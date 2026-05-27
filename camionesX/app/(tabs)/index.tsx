a09c9e06bdf35e5b23699e978808bad2c2c5ccc3
Carlos <73614255+Pidual@users.noreply.github.com>
2026-05-27 00:18:34 -0500

    Expo template
    expo template geneerated using
    
    npx create-expo-app

--- camionesX/app/(tabs)/index.tsx
+++ camionesX/app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">MI PRIMERA APP!</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
